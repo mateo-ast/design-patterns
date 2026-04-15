fc07cac21c5a96d3c20e174d71bd79d195c58e12
mateo-ast <teoastudy@gmail.com>
2026-04-15 19:03:47 -0300

    prototype (#33)

--- src/main/java/design/patterns/creational/prototype/Prototype.java
+++ src/main/java/design/patterns/creational/prototype/Prototype.java
@@ -5,6 +5,24 @@ import design.patterns.DesignPattern;
 public class Prototype implements DesignPattern {
     @Override
     public void run() {
-        System.out.println("Executing pattern: Prototype");
+        // Se crea una tarea base (prototipo)
+        Tarea tareaBase = new Tarea("Estudiar", "Repasar patrones de diseño");
+
+        // Se clona la tarea
+        Tarea tarea1 = (Tarea) tareaBase.clonar();
+
+        // Se modifica la copia
+        tarea1.setNombre("Estudiar Java");
+
+        // Se clona otra vez
+        Tarea tarea2 = (Tarea) tareaBase.clonar();
+
+        // Se modifica la segunda copia
+        tarea2.setNombre("Estudiar Cálculo");
+
+        // Se muestran todas
+        tareaBase.mostrar();
+        tarea1.mostrar();
+        tarea2.mostrar();
     }
 }
