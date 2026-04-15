607657dd12db989fa4b7cdcfa2484dddd8d7114f
Mateo <teoastudy@gmail.com>
2026-04-15 13:16:33 -0300

    adapter

--- src/main/java/design/patterns/structural/adapter/Adapter.java
+++ src/main/java/design/patterns/structural/adapter/Adapter.java
@@ -3,8 +3,27 @@ package design.patterns.structural.adapter;
 import design.patterns.DesignPattern;
 
 public class Adapter implements DesignPattern {
-    @Override
-    public void run() {
-        System.out.println("Executing pattern: Adapter");
+  @Override
+  public void run() {
+    System.out.println("Executing pattern: Adapter");
+    RoundHole hole = new RoundHole(5);
+    RoundPeg rpeg = new RoundPeg(5);
+    if (hole.fits(rpeg)) {
+      System.out.println("La pieza redonda con radio 5 encaja en el agujero redondo con radio 5.");
     }
+
+    SquarePeg smallSqPeg = new SquarePeg(2);
+    SquarePeg largeSqPeg = new SquarePeg(20);
+    // hole.fits(smallSqPeg); // No compila
+
+    SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+    SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+
+    if (hole.fits(smallSqPegAdapter)) {
+      System.out.println("La pieza cuadrada con ancho 2 encaja en el agujero redondo con radio 5.");
+    }
+    if (!hole.fits(largeSqPegAdapter)) {
+      System.out.println("La pieza cuadrada con ancho 20 no encaja en el agujero redondo con radio 5.");
+    }
+  }
 }
