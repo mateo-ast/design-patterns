ae333bb17d9a12a5eed33ca222f280b187dffa74
Mateo <teoastudy@gmail.com>
2026-04-24 20:04:57 -0300

    mediator

--- src/main/java/design/patterns/behavioral/mediator/simple/SimpleMediator.java
+++
@@ -1,9 +0,0 @@
-package design.patterns.behavioral.mediator.simple;
-
-// Mediator que centraliza el envío de mensajes entre usuarios.
-public class SimpleMediator {
-    // Recibe un mensaje de un usuario y lo muestra en consola.
-    public void send(String msg, User user) {
-        System.out.println(user.getName() + " dice: " + msg);
-    }
-}
